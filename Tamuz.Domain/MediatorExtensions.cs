47a9c72464b12e82bf14530367a871c7ea528a6c
Nelson da Silva Neto <nelson.neto@modal.com.br>
2022-06-17 20:36:28 -0300

    Inclusao de Teste

---
+++ Tamuz.Domain/MediatorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+
+namespace Tamuz.Domain
+{
+    public static class MediatorExtensions
+    {
+        public static void AddCustomMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/Tamuz.Api/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Tamuz.Domain;
+
+namespace Tamuz.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCustomMediator();
+
+           //services
+           //    .AddScoped<IParceiroRepository, ParceiroRepository>();
+        }
+    }
+}
