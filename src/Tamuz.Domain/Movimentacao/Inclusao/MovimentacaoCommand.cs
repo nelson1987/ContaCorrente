979b61b1a89c94e2daac4969e48fd00357020459
Nelson da Silva Neto <nelson.neto@modal.com.br>
2022-06-18 10:15:15 -0300

    Inclusao de Movimentacao

--- src/Tamuz.Domain/Movimentacao/Inclusao/MovimentacaoCommand.cs
+++ src/Tamuz.Domain/Movimentacao/Inclusao/MovimentacaoCommand.cs
@@ -1,9 +1,15 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
+using Tamuz.Domain.Repositories;
 
 namespace Tamuz.Domain.Movimentacao.Inclusao
 {
-    public class MovimentacaoNotification: INotification
+    public class ErroNotification
+    {
+        public string Excecao { get; internal set; }
+        public string? PilhaErro { get; internal set; }
+    }
+    public class MovimentacaoIncluidaNotification: INotification
     {
 
     }
@@ -19,20 +25,25 @@ namespace Tamuz.Domain.Movimentacao.Inclusao
     }
     public class MovimentacaoHandler : IRequestHandler<MovimentacaoCommand, MovimentacaoResponse>
     {
-        private readonly ILogger<MovimentacaoHandler> _logger;
-        private readonly IMediator _mediator;
-        public MovimentacaoHandler(ILogger<MovimentacaoHandler> logger, IMediator mediator)
+        private readonly ILogger<MovimentacaoHandler> logger;
+        private readonly IMediator mediator;
+        private readonly IContaRepository contaRepository;
+        public MovimentacaoHandler(ILogger<MovimentacaoHandler> logger, IMediator mediator, IContaRepository contaRepository)
         {
-            _logger = logger;
-            _mediator = mediator;
+            this.logger = logger;
+            this.mediator = mediator;
+            this.contaRepository = contaRepository;
         }
 
         public async Task<MovimentacaoResponse> Handle(MovimentacaoCommand request, CancellationToken cancellationToken)
         {
             try
             {
-                _logger.LogInformation("[INICIO]");
-                _mediator.Send(request);
+                logger.LogInformation("[INICIO]");
+                mediator.Send(request);
+                var movimentacao = new MovimentacaoModel() { };
+                await contaRepository.Add(movimentacao);
+                await mediator.Publish(new MovimentacaoIncluidaNotification { });
                 //var pessoa = new Pessoa { Nome = request.Nome, Idade = request.Idade, Sexo = request.Sexo };
                 //
                 //pessoa = await _repository.Add(pessoa);
@@ -41,11 +52,11 @@ namespace Tamuz.Domain.Movimentacao.Inclusao
 
                 return await Task.FromResult<MovimentacaoResponse>(new MovimentacaoResponse() { });
             }
-            catch
+            catch(Exception ex)
             {
-                //await _mediator.Publish(new PessoaCriadaNotification { Id = pessoa.Id, Nome = pessoa.Nome, Idade = pessoa.Idade, Sexo = pessoa.Sexo });
-                //await _mediator.Publish(new ErroNotification { Excecao = ex.Message, PilhaErro = ex.StackTrace });
-                //return await Task.FromResult("Ocorreu um erro no momento da criação");
+                await mediator.Publish(new MovimentacaoIncluidaNotification { Id = pessoa.Id, Nome = pessoa.Nome, Idade = pessoa.Idade, Sexo = pessoa.Sexo });
+                await mediator.Publish(new ErroNotification { Excecao = ex.Message, PilhaErro = ex.StackTrace });
+                return await Task.FromResult<MovimentacaoResponse>(new MovimentacaoResponse() { });
             }
             throw new NotImplementedException();
         }
